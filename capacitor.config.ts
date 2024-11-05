1adb9082780963a2de9dfecb933360573065b5f5
John Toebes <john@toebes.com>
2024-11-05 10:47:09 -0500

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.toebes.rendergltf',
+    appName: 'rendergltf',
+    webDir: 'dist',
+};
+
+export default config;
